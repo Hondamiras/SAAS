d84e54016aa89591a828ddabd2b6b26e7075e87d
Hondamir <Khondamiras@gmail.com>
2024-12-08 12:18:07 +0500

    Your commit message here

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,13 @@
 
 echo "BUILD START"
 
+# Установить pipenv, если он не установлен
+pip install pipenv
+
 # Установить зависимости из Pipfile
 pipenv install --deploy --ignore-pipfile
 
-# Собрать статические файлы внутри окружения pipenv
+# Собрать статические файлы
 pipenv run python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
