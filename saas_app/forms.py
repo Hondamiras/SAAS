64ca9729cf2719b4699fb879e0735f1391a478e9
Hondamir <157780018+Hondamiras@users.noreply.github.com>
2025-01-08 22:39:14 +0500

    Contact template

--- saas_app/forms.py
+++ saas_app/forms.py
@@ -2,15 +2,12 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'class': 'w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#222]',
-        'placeholder': 'Your Name'
+        'class': 'w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#222]',
     }))
     email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#222]',
-        'placeholder': 'Your Email'
+        'class': 'w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#222]',
     }))
     message = forms.CharField(widget=forms.Textarea(attrs={
-        'class': 'w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#222]',
-        'placeholder': 'Your Message',
+        'class': 'w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#222]',
         'rows': 4
     }))
