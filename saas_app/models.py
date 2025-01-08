e719fef5af8bb54f91c169ac9107787e25f6b0dd
Hondamir <157780018+Hondamiras@users.noreply.github.com>
2025-01-08 14:29:49 +0500

    Changed Product model

--- saas_app/models.py
+++ saas_app/models.py
@@ -101,7 +101,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name='Категория')
-    name = models.CharField(max_length=100, verbose_name='Название')
+    name = models.CharField(max_length=100, verbose_name='Название', null=True, blank=True)
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
