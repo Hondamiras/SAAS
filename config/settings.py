ea50ed602ef7ecda24d4b0b5e7bdf99800952247
Hondamir <157780018+Hondamiras@users.noreply.github.com>
2025-01-12 12:36:19 +0500

    All bugs have been fixed such as forms, texts, icons!

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
-ALLOWED_HOSTS = ['saasteksinvest.uz', 'www.saasteksinvest.uz', '164.90.214.109']
+ALLOWED_HOSTS = ['saasteksinvest.uz', 'www.saasteksinvest.uz', '164.90.214.109', 'saasteks.uz', 'www.saasteks.uz']
 
 # Application definition
 
